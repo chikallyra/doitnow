5ee0b6b653948004e642e615dca50c625e5c1769
chikallyra <chikallyra@gmail.com>
2024-05-20 10:53:52 +0700

    pembaharuan tabel user

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -5,8 +5,6 @@ namespace App\Http\Controllers\Auth;
 namespace App\Http\Controllers;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use App\Providers\RouteServiceProvider;
-namespace App\Http\Controllers;
-
 use Illuminate\Http\Request;
 
 class LoginController extends Controller
@@ -42,7 +40,5 @@ class LoginController extends Controller
         }
     }
 
-    public function index( ) {
-        return view('login.index');
-    }
+
 }

1fde1674b695ef8d346839a1d3bd7e1156997e4e
chikallyra <chikallyra@gmail.com>
2024-05-26 20:29:24 +0700

    upload blogC dan webphp(c)

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -4,7 +4,12 @@ namespace App\Http\Controllers;
 
 use App\Models\Blog;
 use App\Http\Requests\StoreBlogRequest;
+use Illuminate\Http\Request;
 use App\Http\Requests\UpdateBlogRequest;
+use Carbon\Carbon;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Log;
+
 
 class BlogController extends Controller
 {
@@ -15,7 +20,7 @@ class BlogController extends Controller
      */
     public function index()
     {
-        //
+        
     }
 
     /**
@@ -34,9 +39,48 @@ class BlogController extends Controller
      * @param  \App\Http\Requests\StoreBlogRequest  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(StoreBlogRequest $request)
+    public function store(Request $request)
     {
-        //
+        $validatedData = $request->validate([
+            'title' => 'required|max:255',
+            'body' => 'required',
+            'image' => 'nullable|image|mimes:jpeg,png,jpg,gif|max:2048', 
+        ]);
+    
+        // Generate unique slug based on title
+        $slug = Str::slug($validatedData['title']);
+    
+        // Jika ada file gambar di-upload, simpan ke storage dan dapatkan path-nya
+        if ($request->hasFile('image')) {
+            Log::info('File image detected, attempting to store...');
+            try {
+                $imagePath = $request->file('image')->store('images/blogs', 'public');
+                Log::info('Image stored at: ' . $imagePath);
+            } catch (\Exception $e) {
+                Log::error('Error storing image: ' . $e->getMessage());
+                return redirect()->back()->withErrors(['image' => 'Failed to upload image. Please try again.']);
+            }
+        } else {
+            $imagePath = null;
+            Log::info('No image uploaded.');
+        }
+    
+        $currentDate = Carbon::now()->toDateString();
+        $currentTime = Carbon::now()->toTimeString();
+    
+        // Simpan data ke database
+        $blog = new Blog();
+        $blog->title = $validatedData['title'];
+        $blog->slug = $slug;
+        $blog->body = $validatedData['body'];
+        $blog->excerpt = Str::limit(strip_tags($validatedData['body']), 200);
+        $blog->image = $imagePath;
+        $blog->created_date = $currentDate;
+        $blog->created_time = $currentTime;
+        $blog->save();
+    
+        // Redirect dengan pesan sukses
+        return redirect('/admin/home')->with('success', 'Blog successfully created!');
     }
 
     /**
